cefdab390c7ddf2542f0616a8ef5e0089938b390
samikayaa <samikaya89@gmail.com>
2025-07-19 18:26:36 +0300

    c

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,1 +1,10 @@
-console.log('hey'); 
+//console.log('hey'); 
+
+const initialState = 0;
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
