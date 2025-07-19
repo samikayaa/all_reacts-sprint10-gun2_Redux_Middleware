c3318322b9242989b4c25a8942b252ab8835a51f
samikayaa <samikaya89@gmail.com>
2025-07-19 18:47:18 +0300

    C

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,6 @@
 import { movies } from "../../data.js"
 
-/*
-{
+/* {
     id: 8,
     title: 'Memento',
     year: '2000',
@@ -12,8 +11,7 @@ import { movies } from "../../data.js"
     plot: "A man juggles searching for his wife's murderer and keeping his short-term memory loss from being an obstacle.",
     posterUrl:
       'https://www.moviemem.com/wp-content/uploads/2020/07/MEMENTO1SH.jpg',
-}
-*/
+} */
 
 //app.jsx de gördüklerime bakarak initialState oluşturuyorum!
 const initialState = {
@@ -22,6 +20,9 @@ const initialState = {
     sira: 0,
 };
 
+//AKSİYONLARIMIZ;
+//NEXT_MOVIE
+//PREVIOUS_MOVIE
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
         default:
