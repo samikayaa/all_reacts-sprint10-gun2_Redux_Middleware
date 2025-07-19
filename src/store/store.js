524ad6a9ebe54d0e7f6260f8bf516c6e7a144231
samikayaa <samikaya89@gmail.com>
2025-07-19 18:28:10 +0300

    c

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,4 @@
-export const myStore = {}; //kendi storeunuzu create ediniz
+import { legacy_createStore as createStore } from "redux";
+import { reducer } from "./reducers/index"
+
+export const myStore = createStore(reducer); //kendi storeunuzu create ediniz
